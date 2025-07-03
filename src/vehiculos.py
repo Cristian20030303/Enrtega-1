3b5489fa9a64ec4a5f18fd165b19ca7ceed7240a
Cristian20030303 <cristian.murcia@udea.edu.co>
2025-07-02 19:34:29 -0500

    Update vehiculos.py

--- src/vehiculos.py
+++ src/vehiculos.py
@@ -1,1 +1,8 @@
 #Entrada y salida de vehiculos
+vehiculos = []
+
+def ingresar_vehiculo():
+    placa = input("Placa: ").upper()
+    tipo = input("Tipo (carro/moto): ").lower()
+    entrada = timestamp_actual()
+    vehiculos.append({"placa": placa, "tipo": tipo, "entrada": entrada})
