85ab0daed1176993168ef2d646708542dd194c88
Cristian20030303 <cristian.murcia@udea.edu.co>
2025-07-02 19:31:09 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
 # Archivo principal del sistema de gestión de parqueadero
 print("Bienvenido al sistema de parqueadero")
+from src.gestion_usuarios import registrar_usuario
+from src.vehiculos import ingresar_vehiculo
+from src.reportes import generar_reporte
+
+def main():
+    # Aquí pruebas todo lo que vas desarrollando
+    registrar_usuario()
+    ingresar_vehiculo()
+    generar_reporte()
+
+if __name__ == "__main__":
+    main()
