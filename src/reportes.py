010721162f034f79f6a478cdb973190b2e9bb491
Cristian20030303 <cristian.murcia@udea.edu.co>
2025-07-07 23:01:29 -0500

    Update reportes.py

--- src/reportes.py
+++ src/reportes.py
@@ -1,1 +1,2 @@
-#Reportes de usuarios
+# Esto no es, no lo pude eliminar porque lo desarrolle en un carpeta sin darme cuenta que tenia aun archivos innecesarios aqui y
+#despues utilice git para subir y acomodar todo
