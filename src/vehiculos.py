7e8a3a305f19056825225b75e8bde32b2a381595
Cristian20030303 <cristian.murcia@udea.edu.co>
2025-07-07 23:02:24 -0500

    Update vehiculos.py

--- src/vehiculos.py
+++ src/vehiculos.py
@@ -1,8 +1,2 @@
-#Entrada y salida de vehiculos
-vehiculos = []
-
-def ingresar_vehiculo():
-    placa = input("Placa: ").upper()
-    tipo = input("Tipo (carro/moto): ").lower()
-    entrada = timestamp_actual()
-    vehiculos.append({"placa": placa, "tipo": tipo, "entrada": entrada})
+# Esto no es, no lo pude eliminar porque lo desarrolle en un carpeta sin darme cuenta que tenia aun archivos innecesarios aqui y
+#despues utilice git para subir y acomodar todo
