73d8824dc842db2932436b9366d438e7386361a8
Cristian20030303 <cristian.murcia@udea.edu.co>
2025-07-07 22:46:27 -0500

    Delete src directory

--- src/usuarios.py
+++
@@ -1,7 +0,0 @@
-#Registro de usuarios
-usuarios = []
-
-def registrar_usuario():
-    nombre = input("Nombre del usuario: ").title().strip()
-    documento = input("Documento: ").strip()
-    usuarios.append({"nombre": nombre, "documento": documento})
--- src/vehiculos.py
+++
@@ -1,8 +0,0 @@
-#Entrada y salida de vehiculos
-vehiculos = []
-
-def ingresar_vehiculo():
-    placa = input("Placa: ").upper()
-    tipo = input("Tipo (carro/moto): ").lower()
-    entrada = timestamp_actual()
-    vehiculos.append({"placa": placa, "tipo": tipo, "entrada": entrada})
