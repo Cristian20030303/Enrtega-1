91e44727720253c07ceafc5ccaedbb55b45e3cdd
Cristian20030303 <cristian.murcia@udea.edu.co>
2025-07-07 23:01:52 -0500

    Update usuarios.py

--- src/usuarios.py
+++ src/usuarios.py
@@ -1,7 +1,2 @@
-#Registro de usuarios
-usuarios = []
-
-def registrar_usuario():
-    nombre = input("Nombre del usuario: ").title().strip()
-    documento = input("Documento: ").strip()
-    usuarios.append({"nombre": nombre, "documento": documento})
+# Esto no es, no lo pude eliminar porque lo desarrolle en un carpeta sin darme cuenta que tenia aun archivos innecesarios aqui y
+#despues utilice git para subir y acomodar todo
